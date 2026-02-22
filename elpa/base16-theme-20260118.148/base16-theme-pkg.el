028ad66f3f67b8f4d299eeec5946103d790ba4e6
Burak Serpici <bserpici@gmail.com>
2026-02-22 16:45:14 +0300

    gitignore

--- elpa/base16-theme-20260118.148/base16-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "base16-theme" "20260118.148"
-  "Collection of themes built on combinations of 16 base colors."
-  ()
-  :url "https://github.com/tinted-theming/base16-emacs"
-  :commit "1d9457c3940b42a7db586ee9b1249d86d1facbdb"
-  :revdesc "1d9457c3940b"
-  :authors '(("Kaleb Elwert" . "belak@coded.io"))
-  :maintainers '(("Kaleb Elwert" . "belak@coded.io")))
